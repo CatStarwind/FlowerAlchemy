c8be487081481da75adbd154138d62b7ccd2c30b
CatStarwind <catstarwind@gmail.com>
2020-05-04 16:55:53 -0700

    Initialization.

---
+++ js/site.js
@@ -0,0 +1,189 @@
+const genesis = {
+	table: {"rryyWWss":0,"rryyWWSs":2,"rryyWWSS":3,"rryyWwss":4,"rryyWwSs":6,"rryyWwSS":7,"rryywwss":12,"rryywwSs":14,"rryywwSS":15,"rrYyWWss":32,"rrYyWWSs":34,"rrYyWWSS":35,"rrYyWwss":36,"rrYyWwSs":38,"rrYyWwSS":39,"rrYywwss":44,"rrYywwSs":46,"rrYywwSS":47,"rrYYWWss":48,"rrYYWWSs":50,"rrYYWWSS":51,"rrYYWwss":52,"rrYYWwSs":54,"rrYYWwSS":55,"rrYYwwss":60,"rrYYwwSs":62,"rrYYwwSS":63,"RryyWWss":128,"RryyWWSs":130,"RryyWWSS":131,"RryyWwss":132,"RryyWwSs":134,"RryyWwSS":135,"Rryywwss":140,"RryywwSs":142,"RryywwSS":143,"RrYyWWss":160,"RrYyWWSs":162,"RrYyWWSS":163,"RrYyWwss":164,"RrYyWwSs":166,"RrYyWwSS":167,"RrYywwss":172,"RrYywwSs":174,"RrYywwSS":175,"RrYYWWss":176,"RrYYWWSs":178,"RrYYWWSS":179,"RrYYWwss":180,"RrYYWwSs":182,"RrYYWwSS":183,"RrYYwwss":188,"RrYYwwSs":190,"RrYYwwSS":191,"RRyyWWss":192,"RRyyWWSs":194,"RRyyWWSS":195,"RRyyWwss":196,"RRyyWwSs":198,"RRyyWwSS":199,"RRyywwss":204,"RRyywwSs":206,"RRyywwSS":207,"RRYyWWss":224,"RRYyWWSs":226,"RRYyWWSS":227,"RRYyWwss":228,"RRYyWwSs":230,"RRYyWwSS":231,"RRYywwss":236,"RRYywwSs":238,"RRYywwSS":239,"RRYYWWss":240,"RRYYWWSs":242,"RRYYWWSS":243,"RRYYWwss":244,"RRYYWwSs":246,"RRYYWwSS":247,"RRYYwwss":252,"RRYYwwSs":254,"RRYYwwSS":255},
+	bio: ["rryyWWss", "rryyWWSs", "rryyWWSS", "rryyWwss", "rryyWwSs", "rryyWwSS", "rryywwss", "rryywwSs", "rryywwSS", "rrYyWWss", "rrYyWWSs", "rrYyWWSS", "rrYyWwss", "rrYyWwSs", "rrYyWwSS", "rrYywwss", "rrYywwSs", "rrYywwSS", "rrYYWWss", "rrYYWWSs", "rrYYWWSS", "rrYYWwss", "rrYYWwSs", "rrYYWwSS", "rrYYwwss", "rrYYwwSs", "rrYYwwSS", "RryyWWss", "RryyWWSs", "RryyWWSS", "RryyWwss", "RryyWwSs", "RryyWwSS", "Rryywwss", "RryywwSs", "RryywwSS", "RrYyWWss", "RrYyWWSs", "RrYyWWSS", "RrYyWwss", "RrYyWwSs", "RrYyWwSS", "RrYywwss", "RrYywwSs", "RrYywwSS", "RrYYWWss", "RrYYWWSs", "RrYYWWSS", "RrYYWwss", "RrYYWwSs", "RrYYWwSS", "RrYYwwss", "RrYYwwSs", "RrYYwwSS", "RRyyWWss", "RRyyWWSs", "RRyyWWSS", "RRyyWwss", "RRyyWwSs", "RRyyWwSS", "RRyywwss", "RRyywwSs", "RRyywwSS", "RRYyWWss", "RRYyWWSs", "RRYyWWSS", "RRYyWwss", "RRYyWwSs", "RRYyWwSS", "RRYywwss", "RRYywwSs", "RRYywwSS", "RRYYWWss", "RRYYWWSs", "RRYYWWSS", "RRYYWwss", "RRYYWwSs", "RRYYWwSS", "RRYYwwss", "RRYYwwSs", "RRYYwwSS"],
+	bin: [0, 2, 3, 4, 6, 7, 12, 14, 15, 32, 34, 35, 36, 38, 39, 44, 46, 47, 48, 50, 51, 52, 54, 55, 60, 62, 63, 128, 130, 131, 132, 134, 135, 140, 142, 143, 160, 162, 163, 164, 166, 167, 172, 174, 175, 176, 178, 179, 180, 182, 183, 188, 190, 191, 192, 194, 195, 196, 198, 199, 204, 206, 207, 224, 226, 227, 228, 230, 231, 236, 238, 239, 240, 242, 243, 244, 246, 247, 252, 254, 255],
+	color:{
+		"rose": {"red":[128,132,140,164,172,188,194,198,206,228,230,238,254],"yellow":[32,34,35,48,50,51,52,54,55,162,163,178,179,182,183,227,243,247],"white":[0,2,3,4,6,7,36,38,39,60,62,63,131,135,167,191,231,255],"pink":[130,134,142,166,174,190,195,199,207],"orange":[160,176,180,224,226,240,242,244,246],"black":[192,196,204,236],"purple":[12,14,15,44,46,47,143,175,239],"blue":[252]},
+		"tulip": {"red":[140,196,192,228,224],"yellow":[44,36,60,52,48,164,160,180,176],"white":[12,4,0,32,128],"pink":[132],"orange":[172,188],"black":[204,236],"purple":[252,244,240]},
+		"pansy": {"red":[128,132,192,196,224,228],"yellow":[32,36,48,52,60,176,180,188],"white":[0,4],"orange":[160,164,172,240,244],"purple":[204,236,252],"blue":[12,44,140]},
+		"cosmo": {"red":[204,196,192,224,240],"yellow":[44,36,60,52,48],"white":[12,4,0,32],"pink":[140,132,128,160],"orange":[172,164,188,180,176,236,228],"black":[252,244]},
+		"lily": {"red":[140,196,228],"yellow":[44,60,52,164,160,180,176],"white":[12,4,0,36,32,48,128,240],"pink":[132,192,224],"orange":[172,188,252,244],"black":[204,236]},
+		"hyacinth": {"red":[128,192,196,204,228,236],"yellow":[32,36,48,52,60,164,172,180,188],"white":[0,4,44,140],"pink":[132],"orange":[160,176],"purple":[240,244,252],"blue":[12,224]},
+		"windflower": {"red":[140,132,204,196,236,228],"orange":[44,36,60,52,48,188,180,176],"white":[12,4],"pink":[172,164,160,252,244],"purple":[192,224,240],"blue":[0,32,128]},
+		"mum": {"red":[164,192,196,204,236,252],"yellow":[32,36,48,52,60,160],"white":[0,4,44],"pink":[128,132,140,172],"purple":[12,176,180,188,224,228],"green":[240,244]}
+	},
+	order: ['R','r','Y','y','W','w','S','s'],
+	sort: (a,b) => genesis.order.indexOf(a) - genesis.order.indexOf(b),
+	flower: function(species, gene) {		
+		let color = genesis.findColor(species,gene);
+
+		return {
+			'species': species,
+			'color': color,
+			'src': `./img/${species}_${color}.png`,
+			'gene': gene,
+			'seq': genesis.seq(gene)
+		}
+	},
+	seq: function(gene) {
+		let geneSeq = [];
+		for(var n=0;n<gene.length;n+=2){ geneSeq.push(gene.substr(n,2)); }
+		return geneSeq;
+	},
+	findColor: function(species, gene) {
+		for(let color in genesis.color[species]){
+			if(genesis.color[species][color].includes(genesis.table[gene])) return color;
+		}
+	}
+};
+
+const fa = {
+	parent: [],
+	flowerBag: [],
+	placeHolder: '<div class="flower"><div class="phFlower"></div></div>',
+	parseFlower: function(flower) {
+		return genesis.flower($(flower).attr("data-species"), $(flower).attr("data-gene"));
+	},
+	pickParent: function(e) {
+		let flower = fa.parseFlower(this);
+
+		if (fa.parent[0] && !fa.parent[1]) {
+			if (fa.parent[0].species === flower.species){
+				fa.parent[1] = flower;	
+			} else {
+				fa.parent[0] = flower;
+				fa.parent[1] = null;
+				fa.flowerBag = [];
+				fa.drawFlowerBag();
+			}					
+		}
+		else {
+			fa.parent[0] = flower;
+			fa.parent[1] = null;
+			$("#offsprings").html("");
+			if (fa.flowerBag.length && fa.flowerBag[0].species !== flower.species) {
+				fa.flowerBag = [];
+				fa.drawFlowerBag();
+			}
+		}
+
+		fa.drawParent();
+	},
+	pickFlower: function(e) {
+		let flower = fa.parseFlower(this);
+
+		if(!fa.flowerBag.some(f => f.species === flower.species && f.gene === flower.gene)){
+			fa.flowerBag.push(flower);
+		}
+
+		fa.drawFlowerBag();
+	},
+	drawParent: function() {
+		$("#parent1 div").html(fa.drawFlower(fa.parent[0]));
+		$("#parent2 div").html(fa.parent[1] ? fa.drawFlower(fa.parent[1]) : fa.placeHolder);
+	},
+	drawFlower: function(flower){
+		$img = $("<img>").attr("src", flower.src);
+		$gene = $("<div>").addClass("gene").text(flower.gene);
+		$flower = $("<div>").addClass("flower").attr({
+			'data-species': flower.species,
+			'data-gene': flower.gene
+		});
+
+		return $flower.append($img,$gene);
+	},
+	drawFlowerBag: function() {
+		$("#flowerBag").empty();
+		fa.flowerBag.forEach(flower => {
+			$flower = fa.drawFlower(flower).click(fa.pickParent);
+			$("#flowerBag").append($flower);
+		})
+	},
+	breed: function() {
+		if(fa.parent[0] && fa.parent[1]) { 
+			let results = bee.sex(fa.parent[0].seq, fa.parent[1].seq);
+			let species = fa.parent[0].species;
+
+			$table = $("<table>");
+			$table.append("<thead>").append("<tr>").append(
+				$("<th>").text("Flower"),
+				$("<th>").text("Gene"),
+				$("<th>").text("Probability")
+			).append("<tbody>");
+
+			results.forEach(row => {
+				let flower = genesis.flower(species, row.gene);
+				$flower = fa.drawFlower(flower);
+				$flower.click(fa.pickFlower);
+				$flower.prepend($("<span>").text(row.chance+"%")).appendTo("#offsprings");
+			});			
+		}
+	}
+}
+
+const bee = {
+	sex: function(flower1, flower2){
+		let dna1 = bee.rna(flower1);
+		let dna2 = bee.rna(flower2);
+		let result = [];
+
+		dna1.forEach(g1 => {
+			dna2.forEach(g2 => {
+				result.push((g1+g2).split("").sort(genesis.sort).join(""));
+			})
+		});
+
+		let offsprings = [];
+		result.forEach(r => {
+			let i = offsprings.findIndex(dna => dna.gene === r);
+			(i < 0) ? offsprings.push({'gene':r, 'count':1}) : offsprings[i].count++;
+			
+		});
+		offsprings.forEach(child => child.chance = (child.count/result.length)*100);
+		offsprings.sort((a,b) => b.count - a.count || b.gene - a.gene);
+
+		return offsprings;
+	},
+	rna: function(add, base){
+		let trait = add.shift().split("");
+		let newBase = [];
+
+		if(base && base.length) {
+			while(base.length) {
+				let b = base.shift();
+				trait.forEach(a => newBase.push(b+a));
+			}
+		} else {
+			trait.forEach(a=>newBase.push(a));		
+		}
+		
+		if(add.length){ newBase = bee.rna(add,newBase); }
+
+		return newBase;
+	}
+}
+
+const starts = {
+	"Rose": ["RRyyWWSs","rrYYWWss","rryyWwss"],
+	"Tulip": ["RRyyWwss","rrYYwwss","rryyWwss"],
+	"Pansy": ["RRyyWWss","rrYYWWss","rryyWwss"],
+	"Cosmo": ["RRyywwss","rrYYWwss","rryyWwss"],
+	"Lily": ["RRyyWwss","rrYYwwss","rryyWWss"],
+	"Hyacinth": ["RRyyWwss","rrYYWWss","rryyWwss"],
+	"Windflower": ["RRyywwss","rrYYwwss","rryyWwss"],
+	"Mum": ["RRyyWWss","rrYYWWss","rryyWwss"]	
+}
+
+$(function(){
+	for(let start in starts) {
+		if(!$("div.seeds div."+start).length){
+			$("div.seeds").append($("<div>").addClass(start).append(`<h1>${start} Starts</h1>`));
+		}
+		starts[start].forEach(f => {
+			$flower = fa.drawFlower(genesis.flower(start.toLowerCase(), f));
+			$("div.seeds div."+start).append($flower);
+		});
+	}
+
+	$(".seeds .flower").click(fa.pickParent);
+})
